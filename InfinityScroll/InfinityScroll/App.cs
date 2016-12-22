d2d8c963b7f21349dee2d35732a6ef70cc3df94a
pratik8490 <p.kankhara@gmail.com>
2016-12-22 23:49:29 +0530

    scroll

--- InfinityScroll/InfinityScroll/App.cs
+++ InfinityScroll/InfinityScroll/App.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InfinityScroll.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,19 +13,7 @@ namespace InfinityScroll
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new LocationListing();
         }
 
         protected override void OnStart()
